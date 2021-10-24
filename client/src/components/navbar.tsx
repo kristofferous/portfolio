47d6c1cef9156cceae2d8e6813f9426d22fe14f3
Kristoffer <fraithor@gmail.com>
2021-10-24 18:19:24 +0200

    Changes to navbar.tsx

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -4,13 +4,13 @@ function Navbar(this: any, props:any) {
     return (
         <div className={"Navbar"}>
             <div className="navWrapper">
-                <h1 className={"navHome"}>Kristoffer</h1>
+                <a href="/"><h1 className={"navHome"}>Kristoffer</h1></a>
                 <li className={"navList"}>
                     <ul id={"hello"}>Hello</ul>
                     <ul id={"one"}>ONE</ul>
                     <ul id={"two"}>TWO</ul>
                 </li>
-                <h1 className={"navContact"}>Contact me</h1>
+                <h1 className={"navContact"}>Contact</h1>
             </div>
         </div>
     )
